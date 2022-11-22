2b999f8487a5dbd2cde7474a2ce5bc6c57e25986
Dima <d2.grinevich@gmail.com>
2022-11-22 20:20:57 +0300

    refactor

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -5,8 +5,18 @@ import {Header} from "../header/Header";
 import {Particle} from './particles/Particles';
 import {Fade} from 'react-awesome-reveal';
 import Tilt from 'react-parallax-tilt';
+import cv from '../assets/Cv/Cv.pdf'
 
 export const Main = () => {
+    const downloadFile = () => {
+
+      const file = document.createElement('a')
+        file.href  = cv
+        file.setAttribute('download',`Cv.pdf`)
+        document.body.append(file)
+        file.click()
+        file.remove()
+    }
     return (
 
         <div className={style.mainBlock}>
@@ -20,9 +30,7 @@ export const Main = () => {
                             <span>Hello</span>
                             <h1>I’m Dmitry Grinevich</h1>
                             <p>Frontend Developer.</p>
-                            <a href="#">
-                                <button className={style.btn}><span>Check my CV</span></button>
-                            </a>
+                                <button onClick={downloadFile} className={style.btn}><span>Check my CV</span></button>
                         </div>
                         <Tilt className={style.photo} >
                             <div />
